809680878179860f81e108c6e759718629805637
unknown <manaspatil2030@gmail.com>
2021-09-18 18:40:50 +0530

    Added coupon number program

---
+++ CouponNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_Logical_Problems
+{
+    class CouponNumbers
+    {
+        public static void getCouponNum()
+        {
+            Console.WriteLine("Enter the number of coupons to be generated");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int num1;
+            int coupon;
+            
+            for (int i =1; i<=n; i++)
+            {
+                num1 = random.Next(154, 3175);
+                coupon = (1782 * num1) % 1000;
+                Console.WriteLine("coupon {0}-[{1}]",i,coupon);
+
+            }
+
+        }
+    }
+}
