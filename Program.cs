6afd0010f320bcf8c69292d223deab4a6b281a63
unknown <manaspatil2030@gmail.com>
2021-09-18 18:42:43 +0530

    Added Stopwatch to measure the time elapsed from start to end

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day6_Logical_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            Fibonacci_Series.GetFibonacci();
+            PerfectNumber.GetPerfectNum();
+            PrimeNumber.getPrimeNumber();
+            Reverse.getReverse();
+            CouponNumbers.getCouponNum();
+            Console.ReadKey();
+            watch.Stop();
+            var time = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time taken to execute all the codes is {0} milli-seconds", time);
+        }
+    }
+}
